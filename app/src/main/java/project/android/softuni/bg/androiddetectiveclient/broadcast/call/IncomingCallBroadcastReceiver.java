2b6ababd74d7ff3a2ffd32f5e09040e4c6629800
Milko Galev <milkopg@yahoo.com>
2016-09-29 22:46:25 +0300

    try to fix multiple caller receivers issue

--- app/src/main/java/project/android/softuni/bg/androiddetectiveclient/broadcast/call/IncomingCallBroadcastReceiver.java
+++ app/src/main/java/project/android/softuni/bg/androiddetectiveclient/broadcast/call/IncomingCallBroadcastReceiver.java
@@ -3,7 +3,9 @@ package project.android.softuni.bg.androiddetectiveclient.broadcast.call;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.telephony.PhoneStateListener;
+import android.telephony.ServiceState;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
@@ -12,6 +14,7 @@ import com.google.gson.Gson;
 
 import java.util.Date;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import project.android.softuni.bg.androiddetectiveclient.service.DetectiveService;
 import project.android.softuni.bg.androiddetectiveclient.util.Constants;
@@ -27,6 +30,29 @@ public class IncomingCallBroadcastReceiver extends BroadcastReceiver {
   @Override
   public void onReceive(Context context, Intent intent) {
     mContext = context;
+//    long startTime = 0;
+//    long endTime = 0;
+//
+//    Bundle bundle = intent.getExtras();
+//    String number = bundle.getString(Intent.EXTRA_PHONE_NUMBER);
+//
+//    String action = intent.getAction();
+//    if (action.equalsIgnoreCase("android.intent.action.PHONE_STATE")) {
+//      if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(
+//              TelephonyManager.EXTRA_STATE_RINGING)) {
+//        startTime = System.currentTimeMillis();
+//      }
+//      if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(
+//              TelephonyManager.EXTRA_STATE_IDLE)) {
+//        endTime = System.currentTimeMillis();
+//        //Total time talked =
+//        long duration = endTime - startTime;
+//        TimeUnit.MILLISECONDS.toSeconds(duration);
+//        //(duration / 1000) % 60;
+//        //Store total_time somewhere or pass it to an Activity using intent
+//      }
+//    }
+
     TelephonyManager tMgr = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
     IPhoneStateListener listener = new IPhoneStateListener();
     tMgr.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
@@ -54,6 +80,7 @@ public class IncomingCallBroadcastReceiver extends BroadcastReceiver {
         mContext.startService(service);
       }
     }
+
   }
 
 }
